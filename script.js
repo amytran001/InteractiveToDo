4a3702f140bccbfead983b19e33750c9d9c8c639
amytran <167041446+amytran001@users.noreply.github.com>
2024-06-30 22:45:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
-// Selecting elements from the DOM
-const taskForm = document.getElementById('todo-form');
-const taskInput = document.getElementById('task-input');
-const taskList = document.getElementById('task-list');
-
-// Function to create task item
-function createTaskItem(taskName) {
-    const li = document.createElement('li');
-    li.innerText = taskName;
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
-    deleteButton.className = 'delete-btn';
-    deleteButton.addEventListener('click', function() {
-        li.remove();
-    });
-    
-    const completeButton = document.createElement('button');
-    completeButton.innerText = 'Complete';
-    completeButton.className = 'complete-btn';
-    completeButton.addEventListener('click', function() {
-        li.classList.toggle('completed');
-    });
-    
-    li.appendChild(deleteButton);
-    li.appendChild(completeButton);
-    
-    return li;
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-// Event listener for form submission
-taskForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const taskName = taskInput.value.trim();
-    
-    if (taskName !== '') {
-        const taskItem = createTaskItem(taskName);
-        taskList.appendChild(taskItem);
-        taskInput.value = '';
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-});
+  }
+}
